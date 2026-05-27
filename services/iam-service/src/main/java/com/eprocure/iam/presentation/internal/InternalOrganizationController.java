badab6c40e5ad176ed0e987017fa56f51aa4b72b
KLBMinhLong <nguyenminhlongcntt@gmail.com>
2026-05-27 16:44:29 +0700

    feat: add InternalOrganizationController and disable OpenTelemetry tracing in docker-compose

--- services/iam-service/src/main/java/com/eprocure/iam/presentation/internal/InternalOrganizationController.java
+++ services/iam-service/src/main/java/com/eprocure/iam/presentation/internal/InternalOrganizationController.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
