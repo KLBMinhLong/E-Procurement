0f1d9d484dc1410773e90c5c4d7eab3038cdc29c
KLBMinhLong <nguyenminhlongcntt@gmail.com>
2026-06-03 11:16:42 +0700

    feat: add UserDetailsServiceAutoConfiguration in inventory application file

--- services/inventory-service/src/main/java/com/eprocure/inventory/InventoryServiceApplication.java
+++ services/inventory-service/src/main/java/com/eprocure/inventory/InventoryServiceApplication.java
@@ -2,8 +2,9 @@ package com.eprocure.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
